e84b215a29fd32327a232aa2a970176140bb8728
usuario <usuario@com2pc02>
2017-08-17 06:02:51 -0600

    mi primer commit

---
+++ src/main/java/sv/edu/uesocc/ingenieria/prn335_2017/guia01/Utilities.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.uesocc.ingenieria.prn335_2017.guia01;
+
+/**
+ *
+ * @author usuario
+ */
+public class Utilities {
+    
+}
