0616b030d3e05ef317d22df97e13f453027a2d68
usuario <usuario@com2pc02>
2017-08-17 06:22:22 -0600

    metodos creados

--- src/main/java/sv/edu/uesocc/ingenieria/prn335_2017/guia01/Utilities.java
+++ src/main/java/sv/edu/uesocc/ingenieria/prn335_2017/guia01/Utilities.java
@@ -11,4 +11,69 @@ package sv.edu.uesocc.ingenieria.prn335_2017.guia01;
  */
 public class Utilities {
     
+     /**
+     * 
+     * @param texto "cadena de texto  que contiene el titulo de un determinado post"
+     * @return " el texto que se ha recibido  con cada palabara capitalizada  y sin dobles "
+     * 
+     */
+    public String getResume(String texto){
+    
+    String salida = "";
+   if (texto.length() > 30){
+   for ( int i=0; i<30 ; i++){
+   salida += String.valueOf(texto.charAt(i));
+   }
+   
+   }else{
+       salida = texto; 
+               }
+            
+   return salida; 
+    }
+    
+    
+    /**
+     * 
+     * @param texto
+     * @return 
+     */
+    public String capitalizar(String texto){
+   String salida;
+   texto = texto.replaceAll("", "");
+   salida =String.valueOf(texto.charAt(0)).toUpperCase();
+           
+   
+    for( int i  = 1; i < texto.length();i++){
+  // if (texto.charAt(i - 1) == ){
+   
+   
+   
+   
+   }
+   
+   return;
+   
+   }
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    
+    public int contarCoincidencias(String frase, String texto){
+    
+    
+    return;
+    }
+    
 }
+
+
